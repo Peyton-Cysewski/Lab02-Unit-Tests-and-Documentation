74cfb782b4ecc9a9ec7a5fd7140d0cbbf546111e
Peyton Cysewski <pcysewski12@gmail.com>
2020-07-07 19:44:35 -0700

    method and tests for ViewBalance and Withdrawal

--- Lab02-Unit-Tests-and-Documentation/Lab02-Unit-Tests-and-Documentation/Program.cs
+++ Lab02-Unit-Tests-and-Documentation/Lab02-Unit-Tests-and-Documentation/Program.cs
@@ -2,11 +2,35 @@
 
 namespace Lab02_Unit_Tests_and_Documentation
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static public decimal Balance = 10000;
+
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// returns the current value of the account balance
+        /// </summary>
+        /// <returns>balance amount</returns>
+        public static decimal ViewBalance()
+        {
+            return Balance;
+        }
+
+        public static decimal Withdraw(decimal amount)
+        {
+            if (amount < 0 || amount > Balance)
+            {
+                return Balance;
+            }
+            else
+            {
+                Balance = Balance - amount;
+                return Balance;
+            }
         }
     }
 }
--- Lab02-Unit-Tests-and-Documentation/Lab02Tests/UnitTest1.cs
+++ Lab02-Unit-Tests-and-Documentation/Lab02Tests/UnitTest1.cs
@@ -1,14 +1,45 @@
 using System;
 using Xunit;
+using static Lab02_Unit_Tests_and_Documentation.Program;
 
 namespace Lab02Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void DoesViewBalanceWork()
         {
+            Balance = 10000;
 
+            decimal actual = ViewBalance();
+
+            decimal expected = 10000;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void IgnoreNegativeWithdrawal()
+        {
+            Balance = 10000;
+            decimal expected = 10000;
+
+            decimal withdrawalAmount = -5000;
+            decimal actual = Withdraw(withdrawalAmount);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void IgnoreTooLargeWithdrawal()
+        {
+            Balance = 10000;
+            decimal expected = 10000;
+
+            decimal withdrawalAmount = 10001;
+            decimal actual = Withdraw(withdrawalAmount);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
